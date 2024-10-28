ba8ba790d20edb9d7b3a85c3d8c7df35ca207b4c
Abood Magdy <c.aboodmagdy@gmail.com>
2024-10-28 16:42:25 +0300

    feat: healper json respones

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Helpers;
+
+if (!function_exists('responseJson')) {
+    function responseJson($status, $message, $data = null)
+    {
+        $response = [
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ];
+        return response()->json($response);
+    }
+}
